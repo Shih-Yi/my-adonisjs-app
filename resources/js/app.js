c718ede1976a3b85a33f5cf07010b2b5b2950c84
Shih-Yi <brvast@gmail.com>
2024-10-24 14:55:38 +0800

    add layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,22 @@
+// 引入 Bootstrap 的 CSS
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+// 引入 Bootstrap 的 JS 和 Popper.js
+import * as bootstrap from 'bootstrap'
+import '@popperjs/core'
+
+import '../css/app.css'
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
 console.log('Hello World')
+$('h1').text('Hello, World!')
