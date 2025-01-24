1b41c7e1ad47a60323e0bc57f6244a8f70d4c90d
Shih-Yi <brvast@gmail.com>
2025-01-24 16:08:07 +1300

    fix: use admin guard

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -3,20 +3,21 @@ import { HttpContext } from '@adonisjs/core/http'
 import { loginValidator } from '#validators/auth'
 
 export default class AuthController {
-  async showLogin({ view, i18n }: HttpContext) {
+  async showAdminLogin({ view, i18n }: HttpContext) {
     return view.render('auth/login', {
       title: i18n.t('messages.auth.login.title'),
     })
   }
 
-  async login({ request, response, auth, session, i18n }: HttpContext) {
+  async adminLogin({ request, response, auth, session, i18n }: HttpContext) {
     try {
+      // 1. Validate the request data
       const { email, password } = await request.validateUsing(loginValidator)
       // 2. Verify the user's credentials
       const adminUser = await AdminUser.verifyCredentials(email, password)
-      // 3. Login our user
-      await auth.use('web').login(adminUser)
-      return response.redirect().toRoute('/')
+      // 3. Login our user and use admin guard
+      await auth.use('admin').login(adminUser)
+      return response.redirect().toRoute('admin.dashboard')
     } catch (error) {
       session.flash('errors', { form: i18n.t('messages.auth.login.error') })
       return response.redirect().back()
