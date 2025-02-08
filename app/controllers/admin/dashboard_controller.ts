77748ca938b82cfbfe02718fb7dbd5e83239a5bd
Shih-Yi <brvast@gmail.com>
2025-02-08 13:57:39 +1300

    update: admin dashboard

--- app/controllers/admin/dashboard_controller.ts
+++ app/controllers/admin/dashboard_controller.ts
@@ -1,7 +1,12 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
-  public async dashboard({ view }: HttpContext) {
-    return view.render('admin.dashboard')
+  public async dashboard({ view, auth }: HttpContext) {
+    const user = auth.user! // User must exist since passed through auth middleware
+
+    return view.render('admin/dashboard', {
+      user,
+      title: 'Dashboard',
+    })
   }
 }
